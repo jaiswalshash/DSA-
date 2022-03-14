73955cdccf7decb77ee8d08ffc554142619fbb1a
Shashank Jaiswal <71823536+jaiswalshash@users.noreply.github.com>
2022-03-14 23:40:21 +0530

    Add files via upload

---
+++ SDE Sheet/Arrays_4/TwoSum.java
@@ -0,0 +1,22 @@
+package com.company.Arrays_4;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[1] = i;
+                ans[0] = map.get(target - nums[i]);
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+}
